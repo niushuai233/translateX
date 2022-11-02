3c5e6445d7c62eb6b83ad78a80db019cafd88d92
niushuai233 <niushuai951101@gmail.com>
2022-11-02 16:53:00 +0800

    feat: 初始化TranslateX项目

---
+++ src/main/java/cc/niushuai/translatex/TranslateXApplication.java
@@ -0,0 +1,18 @@
+package cc.niushuai.translatex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author niushuai
+ * @date 2022/11/2 16:35
+ */
+@SpringBootApplication
+public class TranslateXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslateXApplication.class);
+    }
+}
